9b3eb6c54aedb17b068b17b30a857112a2a13890
Brandon Ames <Brandon.A.Ames@gmail.com>
2022-03-23 21:34:19 -0500

    Completed Tasks

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (first, second) {
+  if (
+    typeof first !== "number" ||
+    typeof second !== "number" ||
+    first < 0 ||
+    second < 0
+  ) {
+    return "ERROR";
+  }
+  if (first > second) {
+    let temp = second;
+    second = first;
+    first = temp;
+  }
+  let sum = 0;
+  for (let i = first; i <= second; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
