1229e97bb15314722cceb556abb4066d33f4ef01
Brandon Ames <Brandon.A.Ames@gmail.com>
2022-04-08 00:43:12 -0500

    Completed 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  if (!arr.length) return 0;
+  return arr.reduce((total, ele) => total + ele);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((total, ele) => total * ele);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num <= 1) return 1;
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +31,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
