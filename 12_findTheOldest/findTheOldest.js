9219cc31dea7068e4c820000656255cdbc43c75c
Brandon Ames <Brandon.A.Ames@gmail.com>
2022-04-09 12:53:47 -0500

    12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const currentYear = new Date().getFullYear();
+  return people.reduce((oldestPerson, person) => {
+    if (!oldestPerson.yearOfDeath) {
+      oldestPerson.yearOfDeath = currentYear;
+    } else if (!person.yearOfDeath) {
+      person.yearOfDeath = currentYear;
+    }
 
+    if (
+      oldestPerson.yearOfDeath - oldestPerson.yearOfBirth <
+      person.yearOfDeath - person.yearOfBirth
+    ) {
+      oldestPerson = person;
+    }
+    return oldestPerson;
+  });
 };
 
 // Do not edit below this line
