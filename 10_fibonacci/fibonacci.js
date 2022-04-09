6186e8e75fa8881a436d5acdf2601b939c838de5
Brandon Ames <Brandon.A.Ames@gmail.com>
2022-04-09 12:36:21 -0500

    10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (input) {
+  const num = parseInt(input);
+  if (num === 1) return 1;
+  else if (num < 0) return "OOPS";
+  let leftNum = 0;
+  let rightNum = 1;
+  let nextNum = 0;
+  for (let index = 0; index < num - 1; index++) {
+    nextNum = leftNum + rightNum;
+    leftNum = rightNum;
+    rightNum = nextNum;
+  }
+  return nextNum;
 };
 
 // Do not edit below this line
