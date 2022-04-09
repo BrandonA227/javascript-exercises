b3d4fe19225bace645b0446ecdcdc8ca23a7d561
Brandon Ames <Brandon.A.Ames@gmail.com>
2022-04-08 23:53:24 -0500

    09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const strOnlyAlpha = str.toLowerCase().replace(/[^a-z]/g, "");
+  let isPalindrome = true;
+  for (let index = 0; index < Math.floor(strOnlyAlpha.length / 2); index++) {
+    if (strOnlyAlpha[index] !== strOnlyAlpha[strOnlyAlpha.length - index - 1]) {
+      isPalindrome = false;
+    }
+  }
+  return isPalindrome;
 };
 
 // Do not edit below this line
